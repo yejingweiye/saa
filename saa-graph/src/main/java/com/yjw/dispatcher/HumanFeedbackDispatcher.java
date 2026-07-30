46308624eb4d3f4913e2930bf86c4deac6dc3ab5
yejingwei <2868119621@qq.com>
2026-07-30 20:35:52 +0800

    完成节点实现和条件边实现

--- saa-graph/src/main/java/com/yjw/dispatcher/HumanFeedbackDispatcher.java
+++ saa-graph/src/main/java/com/yjw/dispatcher/HumanFeedbackDispatcher.java
@@ -1,11 +1,19 @@
 package com.yjw.dispatcher;
 
 import com.alibaba.cloud.ai.graph.OverAllState;
+import com.alibaba.cloud.ai.graph.StateGraph;
 import com.alibaba.cloud.ai.graph.action.EdgeAction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class HumanFeedbackDispatcher implements EdgeAction {
+
+    private static final Logger logger = LoggerFactory.getLogger(HumanFeedbackDispatcher.class);
+
+
     @Override
     public String apply(OverAllState state) throws Exception {
-        return "";
+        logger.info("HumanFeedbackDispatcher is running");
+        return (String)state.value("human_next_node", StateGraph.END);
     }
 }
--- saa-graph/src/main/java/com/yjw/node/HumanFeedbackNode.java
+++ saa-graph/src/main/java/com/yjw/node/HumanFeedbackNode.java
@@ -1,13 +1,31 @@
 package com.yjw.node;
 
 import com.alibaba.cloud.ai.graph.OverAllState;
+import com.alibaba.cloud.ai.graph.StateGraph;
 import com.alibaba.cloud.ai.graph.action.NodeAction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class HumanFeedbackNode implements NodeAction {
+
+    private static final Logger logger = LoggerFactory.getLogger(HumanFeedbackNode.class);
     @Override
     public Map<String, Object> apply(OverAllState state) throws Exception {
-        return Map.of();
+        logger.info("HumanFeedback node is running");
+        HashMap<String,Object> resultMap = new HashMap<>();
+        String nextStep = StateGraph.END;
+
+        Map<String, Object> feedBackDataMap = state.data();
+        boolean feedBack = (boolean) feedBackDataMap.getOrDefault("feed_back", true);
+        if(feedBack){
+            nextStep = "translate";
+        }
+
+        resultMap.put("human_next_node", nextStep);
+        logger.info("human_feedback node -> {} node", nextStep);
+        return resultMap;
     }
 }
