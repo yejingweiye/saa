d2eed39421bba096042204415a69d05e4c0eef25
yejingwei <2868119621@qq.com>
2026-08-08 02:49:00 +0800

    长期记忆，用户偏好

---
+++ saa-exmple/mem0/src/main/java/com/yjw/Mem0Application.java
@@ -0,0 +1,13 @@
+package com.yjw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mem0Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mem0Application.class, args);
+    }
+
+}
