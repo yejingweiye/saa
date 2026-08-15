a7d367c4cf5a40dba4d9b0a195129de456b06167
yejingwei <2868119621@qq.com>
2026-08-16 02:19:08 +0800

    chat 不同大模型

---
+++ saa-exmple/chat/openai-chat/src/main/java/com/yjw/openai/OpenaiChatApplication.java
@@ -0,0 +1,11 @@
+package com.yjw.openai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenaiChatApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OpenaiChatApplication.class, args);
+    }
+}
